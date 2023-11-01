b6ee99c433857c2a5514375ced0c3d1c5faa5ea3
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-01 12:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //your code here
 let arr=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-int n=arr.length;
+let n=arr.length;
 let non_article=[];
-for(int i=0;i<n;i++){
+for(let i=0;i<n;i++){
 	let str=arr[i];
-	str=str.replace(/\bthe\b|\ban\b|\ba\b/gi,"");
+	str=str.replace(/\bThe\b|\bAn\b|\bA\b/gi,"");
 	str=str.trim();
 	non_article.push(str);
 	
 }
 non_article.sort();
+let names=document.getElementsByTagName('UL')[0];
+names.getElementsByTagName("li")[0].inner HTML=non_article;
 console.log(non_article);
